de5fd7c5aa1b145710c04a3d735c4231cc883a57
AbdelkaderKHEZOUR <94171935+AbdelkaderKHEZOUR@users.noreply.github.com>
2025-04-12 00:51:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,23 +2,38 @@ const todoContainer = document.querySelector('.scroll');
 const todoInput = document.querySelector('#task_input');
 const todoButton = document.querySelector('.add_task');
 const deleteAllButton = document.querySelector('.delete_all');
-const Tasks = document.createElement('li');
-
+const counter = document.querySelector('.counter')
 todoButton.addEventListener('click', addTask);
 deleteAllButton.addEventListener('click', RemoveTask);
-
+let nTask = 0;
+const taskList = [''];
 function addTask() {
+    const Tasks = document.createElement('li');
+
     const task = todoInput.value;
     if (task === '') {
-        alert('a5dam 3la ro7ek');
+        alert('add a valid text');
         return;
-    }
-    Tasks.innerText = task;
+    }    
+    const btn = document.createElement('button');
+        btn.classList.add('removeTask')
+        btn.textContent = 'X'
+    Tasks.textContent = task;
+    Tasks.appendChild(btn);
     todoContainer.appendChild(Tasks);
     todoInput.value = '';
+    nTask ++;
+    counter.textContent = nTask;
+    btn.addEventListener('click',()=>{
+        todoContainer.removeChild(Tasks);
+        nTask = nTask - 1;
+        counter.textContent = nTask
+    });
 }
 function RemoveTask() {
     todoContainer.innerHTML = '';
     todoInput.value = '';
-    alert('man5dmoch ba5alna');
+    alert('All tasks have been deleted');
+    nTask = 0
+    counter.textContent = nTask
 }
